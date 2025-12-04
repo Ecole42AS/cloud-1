0f6ec359466b6c44566bd339d9533bc65b6cb224
Alexandre Stutz <alexandre.stutz@hotmail.com>
2025-12-04 17:29:08 +0100

    gacp

--- inceptionOfThings/p3/scripts/install.sh
+++ inceptionOfThings/p3/scripts/install.sh
@@ -14,7 +14,9 @@ fi
 if ! command -v kubectl &> /dev/null
 then
     echo "kubectl not found. Installing..."
-    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    KUBECTL_VERSION=$(curl -L -s https://dl.k8s.io/release/stable.txt)
+    echo "Downloading kubectl version: $KUBECTL_VERSION"
+    curl -LO "https://dl.k8s.io/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl"
     chmod +x kubectl
     sudo mv kubectl /usr/local/bin/
 else
